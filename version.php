837c254dc76372aa7949b06b2f0d302fdca85554
Isaias Mendes de Oliveira <56656817+isaiasmendesoliveira@users.noreply.github.com>
2026-09-04 13:15:48 -0300

    Add files via upload

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_quizbulkmarks';
-$plugin->version = 2026082600;
-$plugin->release = '0.9.5';
+$plugin->version = 2026090400;
+$plugin->release = '1.0.0';
 $plugin->requires = 2024100700; // Moodle 4.5.
 $plugin->supported = [405, 502];
-$plugin->maturity = MATURITY_RC;
+$plugin->maturity = MATURITY_STABLE;
